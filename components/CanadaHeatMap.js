87f414635a563a2023963d4c8a74a9e72c21f07c
RohiniGaikar <rohini@stuck.ai>
2019-06-04 14:54:18 -0400

    2 files modified, 2 added

--- components/CanadaHeatMap.js
+++ components/CanadaHeatMap.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { MDBContainer } from "mdbreact";
-import { Legend, DataLabel, MapsTooltip } from '@syncfusion/ej2-maps';
-import {MapsComponent } from '@syncfusion/ej2-react-maps';
-import { world_map } from 'world-map.ts';
+//import { Legend, DataLabel, MapsTooltip } from '@syncfusion/ej2-maps';
+//import {MapsComponent } from '@syncfusion/ej2-react-maps';
+//import { world_map } from 'world-map.ts';
 
 import { MapsComponent, LayersDirective, LayerDirective, Inject } from '@syncfusion/ej2-react-maps';
 
 const CanadaHeatMap = () => {
   return(
-    <div>
+    
+            <label className="mt-5" style={{
+              marginLeft:"50px"
+            }}
+            >Canada Heat Map</label>
+            
 
-            <label className="mt-5">Canada Heat Map</label>
-            <MapsComponent id="maps">
-                <LayersDirective>
-                    <LayerDirective shapeData={world_map}>
-                    </LayerDirective>
-                </LayersDirective>
-            </MapsComponent>
-</div>
             
     
   );
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -14,6 +14,7 @@ import DoughnutChart from './DoughnutChart';
 import AllProvinceTable from './AllProvinceTable';
 import CanadaHeatMap from './CanadaHeatMap';
 
+
 const Dashboard = () => {
   return (
     <div style={{
