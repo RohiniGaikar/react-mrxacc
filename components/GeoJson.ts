a496f1861b480d7b8f29ffefcaeef309dab82936
RohiniGaikar <rohini@stuck.ai>
2019-06-04 12:43:23 -0400

    2 files modified, 1 added

---
+++ components/GeoJson.ts
@@ -0,0 +1,10 @@
+let GeoJson = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        { "type": "Feature", "properties": { "iso_3166_2": "MA", "name": "Massachusetts", "admin": "United States of America" }, "geometry": { "type": "MultiPolygon", "coordinates": [[[[-70.801756294617277, 41.248076234530558]]]] }
+        }
+    ]
+    //..
+};
+export default GeoJson;
