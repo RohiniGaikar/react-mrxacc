bf0dd5d5c9fc3192f20cfbc11aeb5353362fb0e1
RohiniGaikar <rohini@stuck.ai>
2019-06-05 00:54:05 -0400

    1 file modified

--- components/ChoroplethMap.js
+++ components/ChoroplethMap.js
@@ -1,7 +1,7 @@
  
  import React, { Component } from 'react';
 import Datamap from 'datamaps/dist/datamaps.world.min.js';
-import d3 from 'd3';
+import  d3 from 'd3';
 //import scale from 'd3';
 import CanadaJson from './Canada.topo.json';
 
